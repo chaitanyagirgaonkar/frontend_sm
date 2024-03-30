458118be5cabd7bff4363da3e51832ffec5bff13
chaitanya <girgaonkar.chaitanya@gmail.com>
2024-03-30 19:24:10 +0530

    vite.config changes

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+// https://eduscribe.onrender.com/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/v1': "http://localhost:8000/api"
+      '/v1': "https://eduscribe.onrender.com/api"
     }
   }
 })
